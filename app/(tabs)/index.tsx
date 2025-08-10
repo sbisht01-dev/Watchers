66c4c1ed090ecaca80cd32b7a920307d46a07d0a
Saurabh Bisht <saurabhb7678@gmail.com>
2025-08-10 15:18:42 +0530

    tab navigation implemented

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center">
-      <Text>Welcome!</Text>
+      <Text className="text-5xl font-bold">Welcome!</Text>
     </View>
   );
 }
